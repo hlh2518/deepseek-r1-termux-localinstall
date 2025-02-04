ace4e6857e0a3b764b640e21b59b490cc1beb337
hlh2518 <39757859+hlh2518@users.noreply.github.com>
2025-02-04 15:04:58 +0800

    Update deepseek-install.sh

--- deepseek-install.sh
+++ deepseek-install.sh
@@ -2,18 +2,18 @@ echo "hshe于2025年2月3日"
 pkg install wget -y
 # 配置参数（根据实际情况调整）
 USE_MIRROR="ghfast.top/https://raw.githubusercontent.com"    # 首选镜像
-#FALLBACK_MIRROR="github.com" # 备用镜像
+
 FORCE_SSL_INSECURE="true"    # 强制跳过SSL验证
 USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36" # 模拟浏览器
 
 # 目标包信息
-DEB_PATH="/hlh2518/deepseek-r1-termux-localinstall/main/deepseek-installer.sh"
-DEB_FILE=$(basename "$DEB_PATH")
+SH_PATH="/hlh2518/deepseek-r1-termux-localinstall/main/deepseek-installer.sh"
+DEB_FILE=$(basename "$SH_PATH")
 
 # 生成镜像URL
 build_url() {
-   echo "https://$1$DEB_PATH"
-    # echo "$1$DEB_PATH"
+   echo "https://$1$SH_PATH"
+    # echo "$1$SH_PATH"
 }
 
 # 智能下载
