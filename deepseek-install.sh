3b446e0007418c378c4f229be9d67b9cc01142f4
hlh2518 <39757859+hlh2518@users.noreply.github.com>
2025-02-04 14:56:52 +0800

    Update deepseek-install.sh

--- deepseek-install.sh
+++ deepseek-install.sh
@@ -1,5 +1,5 @@
 echo "hshe于2025年2月3日"
-pkg install wget -y
+
 # 配置参数（根据实际情况调整）
 USE_MIRROR="ghfast.top/https://raw.githubusercontent.com"    # 首选镜像
 #FALLBACK_MIRROR="github.com" # 备用镜像
@@ -28,12 +28,36 @@ smart_download() {
     
     [ "$FORCE_SSL_INSECURE" = "true" ] && wget_opts+=(--no-check-certificate)
     
-echo "下载正常启动时用的deepseek.sh脚本"
+
+}
+
+
+# 主流程
+main() {
+    # 清理旧文件
+    rm -f "$DEB_FILE" 2>/dev/null
+    
+    # 下载阶段
+    local primary_url=$(build_url "$USE_MIRROR")
+    smart_download "$primary_url"
+    
+    # 验证下载
+    if [ ! -f "$DEB_FILE" ]; then
+        echo "错误：无法下载文件，请检查："
+        echo "1.网络连接状态"
+        echo "2.手动访问测试: $primary_url"
+        echo "3.稍后重试"
+        echo "4.科学上网再试"
+        exit 1
+    fi 
+echo "下载正常启动时用的deepseek-installer.sh脚本"
 sleep 10
+pkg install wget -y
 wget "${wget_opts[@]}" "https://ghfast.top/https://raw.githubusercontent.com/hlh2518/deepseek-r1-termux-localinstall/main/deepseek-installer.sh"
 
 chmod +x deepseek-installer.sh
 ./deepseek-installer.sh
+ 
 }
 
 # 执行主程序
