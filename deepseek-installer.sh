22e7fb2e88ffd3e96098f2f123f35fab210e9826
hlh2518 <39757859+hlh2518@users.noreply.github.com>
2025-02-04 16:01:00 +0800

    Update deepseek-installer.sh

--- deepseek-installer.sh
+++ deepseek-installer.sh
@@ -6,7 +6,7 @@ sleep 10
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list 
 echo "升级..."
 sleep 10
-apt update -y
+#apt update -y
 #apt upgrade -y
 pkg update -y
 pkg upgrade -y
